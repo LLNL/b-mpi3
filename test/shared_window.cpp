9d5cd66d85f8bf1432da35a182a1ec25e17b9e45
correaa <correaa@llnl.gov>
2017-06-29 17:30:34 +0200

    simplified shared window test

--- test/shared_window.cpp
+++ test/shared_window.cpp
@@ -1,5 +1,5 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 2s $0x.x $@ && rm -f $0x.x; exit
+mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 5 $0x.x $@ && rm -f $0x.x; exit
 #endif
 
 #include "alf/boost/mpi3/main.hpp"
@@ -9,24 +9,24 @@ mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 2s $0x.x 
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-#define ARRAY_LEN 1000
-
 int mpi3::main(int argc, char *argv[], mpi3::communicator& world){
 
-	mpi3::shared_window sw(world, world.rank()==0?ARRAY_LEN*sizeof(double):0);
+	int size = 100;
+	mpi3::shared_window sw(world, world.rank()==0?100*sizeof(double):0);
 	int* arr = (int*)sw.base(0);
+//	int size = sw.size(0)/sizeof(double);
 
 	cout << "In rank " << world.rank() << " baseptr = " << arr << std::endl;
 
-	for(int i = world.rank(); i < ARRAY_LEN; i+=world.size())
+	for(int i = world.rank(); i < size; i+=world.size())
 		arr[i] = world.rank();
 
 	world.barrier();
+
 	if(world.rank() == 0){
-		for(int i = 0; i < 10; i++) cout << arr[i];
+		for(int i = 0; i < size; i++) cout << arr[i];
 		cout << '\n';
 	}
-	world.barrier();
 
 	return 0;
 }
