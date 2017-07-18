92310bd547b58d9322fdd51daf5856622b6487a1
correaa <correaa@llnl.gov>
2017-07-18 17:07:16 -0600

    managed shared memory example made to work

--- shm/managed_shared_memory.hpp
+++ shm/managed_shared_memory.hpp
@@ -1,6 +1,7 @@
 #if COMPILATION_INSTRUCTIONS
-(echo "#include<"$0">" > $0x.cpp) && mpicxx -O3 -std=c++14 -Wfatal-errors -D_TEST_BOOST_MPI3_SHM_MANAGED_SHARED_MEMORY $0x.cpp -o $0x.x -lrt -lboost_system && time mpirun -np 10 $0x.x $@ && rm -f $0x.x $0x.cpp; exit
+(echo "#include<"$0">" > $0x.cpp) && time mpicxx -O3 -std=c++14 -Wfatal-errors -D_TEST_BOOST_MPI3_SHM_MANAGED_SHARED_MEMORY $0x.cpp -o $0x.x -lrt -lboost_system && time mpirun -np 6 $0x.x $@ && rm -f $0x.x $0x.cpp; exit
 #endif
+
 #ifndef BOOST_MPI3_SHM_MANAGED_SHARED_MEMORY_HPP
 #define BOOST_MPI3_SHM_MANAGED_SHARED_MEMORY_HPP
 
@@ -24,6 +25,8 @@ namespace shm{
 struct managed_shared_memory{
 	communicator& comm_;
 	shared_window sw_;
+	using size_type = mpi3::size_t;
+	using difference_type = mpi3::size_t;
 	boost::simple_segregated_storage<std::size_t> storage_;
 	managed_shared_memory(communicator& comm, mpi3::size_t n) : 
 		comm_(comm), 
@@ -34,6 +37,14 @@ struct managed_shared_memory{
 	template<class T>
 	T* malloc(){
 		return (T*)storage_.malloc_n(1, sizeof(T));
+	}
+	template<class T>
+	T* malloc_n(mpi3::size_t n){
+		return (T*)storage_.malloc_n(1, n*sizeof(T));
+	}
+	template<class T>
+	void free_n(T* ptr, mpi3::size_t n){
+		storage_.free_n(ptr, 1, n*sizeof(T));
 	}
 	template<class T, class... Args>
 	T* construct(Args&&... args){
@@ -47,19 +58,54 @@ struct managed_shared_memory{
 		return p;
 	}
 	template<class T>
-	void destroy(T* ptr){
+	void destroy_ptr(T* ptr){
 		sw_.lock_exclusive(0);
 		if(comm_.rank()==0) ptr->~T();
 		storage_.free_n(ptr, 1, sizeof(T));
 		sw_.unlock(0);
 	}
+	template<class T>
+	void destroy(T& t){destroy_ptr(&t);}
 };
+
+template<class T> struct allocator{
+	managed_shared_memory& msm_;
+
+	using value_type = T;
+	using pointer = T*;
+	using const_pointer = T const*;
+	using reference = T&;
+	using const_reference = T const&;
+	using size_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
+
+	allocator(managed_shared_memory& msm) : msm_(msm){}
+
+	template<class U> 
+	allocator(allocator<U> const& other) : msm_(other.msm_){}
+
+	pointer allocate(size_type n, const void* hint = 0){
+		return pointer(msm_.malloc_n<T>(n));
+	}
+	void deallocate(pointer ptr, size_type n){msm_.free_n<T>(ptr, n);}
+	bool operator==(allocator const& other) const{
+		return msm_ == other.msm_;
+	}
+	bool operator!=(allocator const& other) const{
+		return not (other == *this);
+	}
+};
+
 
 }}}
 
 #ifdef _TEST_BOOST_MPI3_SHM_MANAGED_SHARED_MEMORY
 
 #include "alf/boost/mpi3/main.hpp"
+#include "alf/boost/mpi3/mutex.hpp"
+#include <boost/pool/pool_alloc.hpp>
+#include <boost/container/string.hpp>
+
 #include<atomic>
 
 namespace mpi3 = boost::mpi3;
@@ -67,21 +113,63 @@ using std::cout;
 
 int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
 
-	mpi3::shm::managed_shared_memory msm(world, 10000);
+	mpi3::communicator node = world.split_shared(0);
 
-	std::atomic<int>& i = *msm.construct<std::atomic<int>>(0);
-	world.barrier();
+	int N = 10;
+	mpi3::shm::managed_shared_memory msm(node, 100000);
 
-	++i; std::this_thread::sleep_for(std::chrono::seconds(rand(10)));
-
-	if(world.rank() == 0){
-		int snapshot = i;
-		cout << "snapshot " << snapshot << " size " << world.size() << std::endl;
+	node.barrier();
+	{ // using atomics
+		std::atomic<int>& i = *msm.construct<std::atomic<int>>(0);
+		node.barrier();
+		for(int j = 0; j != N; ++j) ++i; // std::this_thread::sleep_for(std::chrono::seconds(rand(10)));
+		node.barrier();
+		if(node.rank() == 0){
+			int snapshot = i;
+			assert( snapshot == node.size()*N );
+			cout << "snapshot " << snapshot << " size " << node.size()*N << std::endl;
+		}
+		node.barrier();
+		msm.destroy_ptr<std::atomic<int>>(&i);
 	}
-
-	world.barrier();
-	msm.destroy<std::atomic<int>>(&i);
-
+	node.barrier();
+	{ // not using atomics
+		int& i = *msm.construct<int>(0);
+		node.barrier();
+		for(int j = 0; j != N; ++j) ++i; // std::this_thread::sleep_for(std::chrono::seconds(rand(10)));
+		node.barrier();
+		if(node.rank() == 0){
+			int snapshot = i;
+			cout << "snapshot " << snapshot << " size " << node.size()*N << std::endl;
+			assert( snapshot <= node.size()*N ); // not using atomics (or mutexes) can lead to incorrect sums
+		}
+		node.barrier();
+		msm.destroy_ptr<int>(&i);
+	}
+	node.barrier();
+	{
+		mpi3::shm::allocator<char> ca(msm);
+		using String = boost::container::basic_string<char, std::char_traits<char>, mpi3::shm::allocator<char>>; 
+		String& s = *msm.construct<String>("Hello!", ca);
+		if(node.rank() == 1) assert( s[1] == 'e' );
+		node.barrier();
+		s[node.rank()] = 'a' + node.rank();
+		node.barrier();
+		if(node.rank()==0) cout << s << std::endl;
+		node.barrier();
+		msm.destroy_ptr<String>(&s);
+	}
+	node.barrier();
+	{
+		mpi3::shm::allocator<double> da(msm);
+		using Vector = boost::container::vector<double, mpi3::shm::allocator<double>>;
+		Vector& v = *msm.construct<Vector>(node.size(), 1., da);
+	//	v[world.rank()] = world.rank();
+		world.barrier();
+	//	assert( v[8] == 8. );
+		msm.destroy_ptr<Vector>(&v);
+	}
+	node.barrier();
 	return 0;
 }
 #endif
