e522605af20b627250886e33665e5c0ed1376d94
correaa <correaa@llnl.gov>
2017-10-01 22:07:38 -0700

    use MPI_Get_Address rather than deprecated MPI_Address function

--- address.hpp
+++ address.hpp
@@ -1,5 +1,5 @@
 #if COMPILATION_INSTRUCTIONS
-(echo "#include<"$0">" > $0x.cpp) && mpicxx -O3 -std=c++14 -Wfatal-errors -D_TEST_BOOST_MPI3_ADDRESS -lboost_mpi -lboost_serialization -lboost_timer $0x.cpp -o $0x.x && time mpirun -np 4 $0x.x $@ && rm -f $0x.cpp; exit
+(echo "#include<"$0">" > $0x.cpp) && mpicxx -O3 -std=c++14 -Wfatal-errors -D_TEST_BOOST_MPI3_ADDRESS -lboost_serialization -lboost_timer $0x.cpp -o $0x.x && time mpirun -np 4 $0x.x $@ && rm -f $0x.cpp; exit
 #endif
 #ifndef BOOST_MPI3_ADDRESS_HPP
 #define BOOST_MPI3_ADDRESS_HPP
@@ -15,10 +15,11 @@ namespace boost{
 namespace mpi3{
 
 using address = MPI_Aint;
+using size_t = MPI_Aint;
 
 address get_address(void const* location){
 	address ret = -1;
-	int status = MPI_Address(location, &ret);
+	int status = MPI_Get_address(location, &ret); // MPI_Address is deprecated
 	if(status != 0) throw std::runtime_error("error taking address");
 	return ret;
 }
@@ -33,11 +34,12 @@ address addressof(T const& t){return get_address(std::addressof(t));}
 #include "alf/boost/mpi3/main.hpp"
 
 using std::cout;
+namespace mpi3 = boost::mpi3;
 
-int boost::mpi3::main(int argc, char* argv[], boost::mpi3::communicator& world){
+int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
 	std::vector<int> v(10);
-	boost::mpi3::address a1 = boost::mpi3::addressof(v[0]);
-	boost::mpi3::address a2 = boost::mpi3::addressof(v[1]);
+	mpi3::address a1 = mpi3::addressof(v[0]);
+	mpi3::address a2 = mpi3::addressof(v[1]);
 	assert( a2 - a1 == sizeof(int) ); 
 }
 
