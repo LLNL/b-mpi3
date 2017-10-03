2f92302a416ee5193cc29c8d9ee07909cee18afa
correaa <correaa@llnl.gov>
2017-10-02 21:50:29 -0700

    shared comm example added type info to shared window

--- shared_communicator.hpp
+++ shared_communicator.hpp
@@ -13,6 +13,12 @@ template<class T = void>
 struct shared_window;
 
 struct shared_communicator : communicator{
+	private:
+	template<class T> static auto data_(T&& t){
+		using detail::data;
+		return data(std::forward<T>(t));
+	}
+	public:
 	shared_communicator(communicator const& comm, int key = 0){
 		int s = MPI_Comm_split_type(comm.impl_, MPI_COMM_TYPE_SHARED, key,  MPI_INFO_NULL, &impl_);
 		if(s != MPI_SUCCESS) throw std::runtime_error("cannot split shared");
@@ -23,13 +29,20 @@ struct shared_communicator : communicator{
 	shared_window<T> make_shared_window();
 	template<
 		class It1, class Size, class Op, 
-		class V1 = typename std::iterator_traits<It1>::value_type, 			class P1 = decltype(detail::data(It1{})), 
+		class V1 = typename std::iterator_traits<It1>::value_type, class P1 = decltype(data_(It1{})), 
+		class PredefinedOp = predefined_operation<Op>
+	>
+	auto all_reduce_in_place_n(It1 first, Size count, Op op){
+		int s = MPI_Allreduce(MPI_IN_PLACE, data_(first), count, detail::basic_datatype<V1>{}, PredefinedOp{}, impl_);
+		if(s != MPI_SUCCESS) throw std::runtime_error("cannot reduce n");
+	}
+	template<
+		class It1, class Size, class Op, 
+		class V1 = typename std::iterator_traits<It1>::value_type, class P1 = decltype(detail::data(It1{})), 
 		class PredefinedOp = predefined_operation<Op>
 	>
 	auto all_reduce_n(It1 first, Size count, Op op){
-		using detail::data;
-		int s = MPI_Allreduce(MPI_IN_PLACE, data(first), count, detail::basic_datatype<V1>{}, PredefinedOp{}, impl_);
-		if(s != MPI_SUCCESS) throw std::runtime_error("cannot reduce n");
+		return all_reduce_in_place_n(first, count, op);
 	}
 };
 
@@ -52,20 +65,19 @@ using std::cout;
 
 int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
 	mpi3::shared_communicator node = world.split_shared();
-	mpi3::shared_window win = node.make_shared_window<int>(node.rank()?0:1);
+	mpi3::shared_window<int> win = node.make_shared_window<int>(node.rank()?0:1);
 	assert(win.base() != nullptr and win.size<int>() == 1);
 	win.lock_all();
 	if(node.rank()==0){
 		*win.base<int>() = 42;
 		win.sync();
 	}
-	for (int j=1; j != node.size(); ++j) {
-	    if (node.rank()==0) node.send_n((int*)nullptr, 0, j, 666);
-	    else if (node.rank()==j) node.receive_n((int*)nullptr, 0, 0, 666);
+	for(int j=1; j != node.size(); ++j){
+		if (node.rank()==0) node.send_n((int*)nullptr, 0, j, 666);
+		else if (node.rank()==j) node.receive_n((int*)nullptr, 0, 0, 666);
 	}
-	if(node.rank()!=0){
-		win.sync();
-	}
+	if(node.rank()!=0) win.sync();
+
 	int l = *win.base<int>();
 	win.unlock_all();
 
