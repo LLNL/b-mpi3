4b5efe17db3b31d00cd0486d646a7ff0e3e9ba5c
correaa <correaa@llnl.gov>
2017-10-24 23:34:30 -0700

    fixed bug in comm send test

--- test/communicator_send.cpp
+++ test/communicator_send.cpp
@@ -11,24 +11,27 @@ namespace mpi3 = boost::mpi3;
 using std::cout;
 
 
-int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
+int mpi3::main(int, char*[], mpi3::communicator& world){
 
 	using type = std::complex<double>;
+	int N = 10;
+	
+	assert( world.size() == 2 );
 
-	assert( world.size() == 2);
-
-	std::vector<type> buffer(10);
+	std::vector<type> buffer(N);
 	iota(buffer.begin(), buffer.end(), double{0.});
 
 	if(world.rank()==0){
 		std::vector<type> const& cbuffer = buffer;
 		world.send(cbuffer.begin(), cbuffer.end(), 1, 123);
 	}else{
-		std::vector<type> buffer2(10);
-	//	world.receive(buffer2.begin(), buffer2.end(), 0, 123);
-		world.receive(buffer2.begin());//, 0, 123);
+		std::vector<type> buffer2(N);
+		world.receive(buffer2.begin(), buffer2.end(), 0, 123);
+	//	world.receive(buffer2.begin());//, 0, 123);
 		assert( buffer == buffer2 );
 	}
 	cout << "finished" << std::endl;
+	return 0;
+
 }
 
