d9daec457167bc5f411a466ba652d29d6d0d8c30
correaa <correaa@llnl.gov>
2017-10-24 21:51:43 -0700

    test of communicator divide

---
+++ test/communicator_divide.cpp
@@ -0,0 +1,28 @@
+#if COMPILATION_INSTRUCTIONS
+mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 8 $0x.x $@ && rm -f $0x.x; exit
+#endif
+
+#include "alf/boost/mpi3/main.hpp"
+#include "alf/boost/mpi3/communicator.hpp"
+
+#include <chrono> //literals
+#include <thread> //sleep_for
+
+namespace mpi3 = boost::mpi3;
+using std::cout;
+
+int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
+
+	assert( world.size() == 8 );
+	
+	mpi3::communicator third = world / 3;
+
+	cout 
+		<< "I am rank " << world.rank() << " in " << world.name() << ", "
+		<< "I am also " << third.rank() << " in " << third.name() << '\n'
+	;
+	
+	
+	return 0;
+}
+
