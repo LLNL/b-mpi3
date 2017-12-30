6c69c4f06264b50bcd9ffbbc586c3fc85fad21d3
correaa <correaa@llnl.gov>
2017-12-29 22:24:41 -0800

    example showing window and subcommunicator creating with comparators

--- test/window_put_get.cpp
+++ test/window_put_get.cpp
@@ -1,47 +1,33 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 4 $0x.x $@ && rm -f $0x.x; exit
+mpic++ -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 4 $0x.x $@ && rm -f $0x.x; exit
 #endif
 
 #include "alf/boost/mpi3/main.hpp"
 #include "alf/boost/mpi3/window.hpp"
 
 #include<cassert>
+#include<iostream>
 
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-#define NROWS 100
-#define NCOLS 100
 
-#include<iostream>
-
-template<int N>
-int no_of_ele(int (&A)[N]){
-    return sizeof(A)/sizeof(A[0]);
-}
-
-int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
-
-    int arr[5] = {1,2,3,4,5};
-
-    std::cout<<"Number of elements in array are "<<no_of_ele(arr)<<std::endl;
-
+int mpi3::main(int, char*[], mpi3::communicator& world){
 
 	std::vector<double> inbuf(100);
 	std::vector<double> outbuf(100);
 	
 	std::iota(outbuf.begin(), outbuf.end(), 0.0);
 
-	mpi3::communicator comm = (world <= 1);
-	if(world.rank() > 1) return 0;
+	mpi3::communicator comm = (world < 2);
+	if(not comm) return 0;
 
 	std::iota(outbuf.begin(), outbuf.end(), 0.0);
 
-	mpi3::window w;
-	if(world.rank() == 0) 
-		w = mpi3::window(comm);
-	else if(world.rank() == 1) 
-		w = mpi3::window(inbuf.data(), inbuf.size(), comm);
+	mpi3::window<double> w = comm.make_window(
+		comm.rank() == 1?inbuf.data():nullptr,
+		comm.rank() == 1?inbuf.size():0
+	);
 	w.fence();
 	if(world.rank() == 0) w.put_n(outbuf.data(), outbuf.size(), 1);
 	w.fence();
