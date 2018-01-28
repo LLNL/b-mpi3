7e64a8ba591f6854c4dba0d6db477edc05b7aef0
Alfredo Correa <alfredo@inspiron.attlocal.net>
2018-01-28 03:12:23 -0800

    added fake examples and make the lib work with send and recv

---
+++ fake/examples/abort.cpp
@@ -0,0 +1,9 @@
+#include <mpi.h>
+int main(int argc, char *argv[])
+{
+    MPI_Init(NULL, NULL);
+    MPI_Abort(MPI_COMM_WORLD, 911);
+    /* No further code will execute */
+    MPI_Finalize();
+    return 0;
+}
