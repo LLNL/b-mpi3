f5fea034079c6e9ca6d2477712018e2994cb5718
correaa <correaa@llnl.gov>
2018-01-30 19:07:27 -0800

    construct no guarded, destructor is guarded

--- shared_window.hpp
+++ shared_window.hpp
@@ -174,10 +174,12 @@ template<class T> struct allocator{
 	}
 	template<class U, class... Args>
 	void construct(U* p, Args&&... args){
-		if(comm_.root()) ::new((void*)p) U(std::forward<Args>(args)...);
+	//	if(comm_.root())
+		::new((void*)p) U(std::forward<Args>(args)...);
 	}
-	template< class U >
-	void destroy(U* p){if(comm_.root()) p->~U();}
+	template< class U >	void destroy(U* p){
+		if(comm_.root()) p->~U();
+	}
 };
 
 }
