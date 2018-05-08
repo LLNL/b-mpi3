e6e62d5270a0e475e820fd7e45ad6204b7581ff1
Alfredo Correa <correaa@llnl.gov>
2018-05-08 10:47:54 -0700

    initial test script

--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,32 @@
 #if COMPILATION_INSTRUCTIONS
-c++ -std=c++11 -O3 -Wall -Wextra -Wfatal-errors $0 -o $0x.x -lstdc++fs && $0x.x $@  ; rm -f $0x.x; exit
+c++ -std=c++14 -O3 -Wall -Wextra -Wfatal-errors $0 -o $0x.x -lstdc++fs && $0x.x $@  ; rm -f $0x.x; exit
 #endif
 
 
 #include<iostream>
 #include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+#include<string>
 
-int main(){
-	for(fs::path const& p: fs::directory_iterator("./")){
-		if(p == "./test.cpp") continue;
-		std::cout << p << '\n';
+namespace fs = std::experimental::filesystem;
+using namespace std::string_literals;
+using std::cout;
+
+int main(int argc, char* argv[]){
+	std::vector<fs::path> some;
+	if(argc == 1 or argv[1] == "all"s){
+		for(fs::path const& p: fs::directory_iterator("./")){
+			if(p == "./test.cpp") continue;
+			some.push_back(p);
+		}
+	}else if(argc==2 and argv[1] == "some"s){
+		some = {
+			"./communicator_split.cpp", 
+			"./communicator_send.cpp"
+		};
+	}
+	for(fs::path const& p: some){
+		cout << p << '\n';
 		std::system(("sh " + p.string()).c_str());
 	}
 }
+
