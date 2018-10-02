a2d4229a526f997533cd2968f2cd0241afd88486
Alfredo Correa <correaa@llnl.gov>
2018-10-02 14:40:48 -0700

    improved tests

--- test/communicator_barrier.cpp
+++ test/communicator_barrier.cpp
@@ -1,16 +1,17 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 -Wall -Wfatal-errors $0 -o $0x.x && time mpirun -np 4s $0x.x $@ && rm -f $0x.x; exit
+mpic++ -O3 -std=c++14 -Wall -Wfatal-errors $0 -o $0x.x && time mpirun -n 4 $0x.x $@ && rm -f $0x.x; exit
 #endif
-
-#include "alf/boost/mpi3/main.hpp"
-#include "alf/boost/mpi3/communicator.hpp"
+//  (C) Copyright Alfredo A. Correa 2018.
+#include "../../mpi3/main.hpp"
+#include "../../mpi3/communicator.hpp"
 
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
+int mpi3::main(int, char*[], mpi3::communicator world){
+	cout <<"Before barrier, I am "<< world.rank() <<" of "<< world.size() << std::endl;
 	world.barrier();
-	cout << "I am " << world.rank() << " of " << world.size() << '\n';
+	cout <<"After barrier, I am "<< world.rank() <<" of "<< world.size() << std::endl;
 	return 0;
 }
 
--- test/communicator_broadcast.cpp
+++ test/communicator_broadcast.cpp
@@ -1,7 +1,7 @@
 #if COMPILATION_INSTRUCTIONS
 mpic++ -O3 -std=c++14 -Wall -Wfatal-errors $0 -o $0x.x -lboost_serialization && time mpirun -np 2 $0x.x $@ && rm -f $0x.x; exit
 #endif
-
+//  (C) Copyright Alfredo A. Correa 2018.
 #include "../../mpi3/main.hpp"
 #include "../../mpi3/communicator.hpp"
 #include "../../mpi3/process.hpp"
@@ -10,7 +10,7 @@ mpic++ -O3 -std=c++14 -Wall -Wfatal-errors $0 -o $0x.x -lboost_serialization && 
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-int mpi3::main(int argc, char* argv[], mpi3::communicator world){
+int mpi3::main(int, char*[], mpi3::communicator world){
 
 	std::vector<int> sizes = { 100, 64*1024, 128*1024 };
 	int NUM_REPS = 5;
