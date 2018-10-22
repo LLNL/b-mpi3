74a396dbf07ae4d75a66e5f45ec155cf75e56eb8
Alfredo Correa <correaa@llnl.gov>
2018-10-22 15:56:21 -0700

    improved ireceive_n

--- test/communicator_operator.cpp
+++ test/communicator_operator.cpp
@@ -1,9 +1,9 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 8 $0x.x $@ && rm -f $0x.x; exit
+mpic++ -O3 -std=c++14 -Wall -Wextra -Wfatal-errors $0 -o $0x.x && time mpirun -n 2 $0x.x $@ && rm -f $0x.x; exit
 #endif
 
-#include "alf/boost/mpi3/main.hpp"
-#include "alf/boost/mpi3/communicator.hpp"
+#include "../../mpi3/main.hpp"
+#include "../../mpi3/communicator.hpp"
 
 #include <chrono> //literals
 #include <thread> //sleep_for
@@ -13,36 +13,31 @@ using std::cout;
 using namespace std::chrono_literals;
 
 
-int mpi3::main(int argc, char* argv[], mpi3::communicator& world){
+int mpi3::main(int, char*[], mpi3::communicator world){
 
 	std::vector<double> inbuf(100);
 	std::vector<double> outbuf(100);
 
-	assert(world.size() == 8);
-	mpi3::communicator comm = (world <= 1);
-	assert(comm.size() == 6 or comm.size() == 2);
-	if(world.rank() > 1) return 0;
-	assert(comm.size() == 2);
-
-	std::cout << "world[" << world.rank() << "] is also comm[" << comm.rank() << "]" << std::endl;
 	mpi3::request r;
-	if(world.rank() == 0){
-		std::iota(outbuf.begin(), outbuf.end(), 0.0);
-		std::this_thread::sleep_for(10s);
-		std::cout << "comm[" << comm.rank() << "] about to isent" << std::endl;
-		r = comm.isend(outbuf.begin(), outbuf.end(), 1);
-		std::cout << "comm[" << comm.rank() << "] isent" << std::endl;
-	}else if(world.rank() == 1){
-		std::cout << "comm[" << comm.rank() << "] about to ireceive" << std::endl;
-		r = comm.ireceive(inbuf.begin(), inbuf.end(), 0);
-		std::cout << "comm[" << comm.rank() << "] ireceive" << std::endl;
+	switch(world.rank()){
+		case 0: {
+			iota(begin(outbuf), end(outbuf), 0.0);
+			std::this_thread::sleep_for(2s);
+			cout <<"world["<< world.rank() <<"] about to isent"<< std::endl;
+			r = world.isend(begin(outbuf), end(outbuf), 1);
+			cout <<"comm["<< world.rank() <<"] isent"<< std::endl;
+			r.wait();
+		}; break;
+		case 1: {
+			cout <<"comm["<< world.rank() <<"] about to ireceive"<< std::endl;
+			r = world.ireceive_n(inbuf.begin(), inbuf.size(), 0);
+			cout <<"comm["<< world.rank() <<"] ireceived"<< std::endl;
+			r.wait();
+		}
 	}
-	r.wait();
-	std::cout << "comm[" << comm.rank() << "] completed op" << std::endl;
+	cout <<"comm["<< world.rank() <<"] completed op"<< std::endl;
 
 	if(world.rank() == 1) assert( inbuf[9] == 9. );
-	
-	
 
 	return 0;
 }
