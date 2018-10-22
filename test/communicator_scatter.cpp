8ea4734a9bb6af45be5c06c3400e4fe4bdd71999
Alfredo Correa <correaa@llnl.gov>
2018-10-22 12:19:26 -0700

    test scatter and scatter_from syntax

--- test/communicator_scatter.cpp
+++ test/communicator_scatter.cpp
@@ -1,32 +1,40 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x && time mpirun -np 8s $0x.x $@ && rm -f $0x.x; exit
+mpic++ -O3 -std=c++14 -Wall -Wextra $0 -o $0x.x && time mpirun -n 8 $0x.x $@ && rm -f $0x.x; exit
 #endif
 
-#include "alf/boost/mpi3/environment.hpp"
-#include "alf/boost/mpi3/communicator.hpp"
+#include "../../mpi3/environment.hpp"
+#include "../../mpi3/communicator.hpp"
 
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-int main(int argc, char* argv[]){
+#define N 8
+
+int main(){
+
 	mpi3::environment env;
-	mpi3::communicator& world = env.world();
+	mpi3::communicator world = env.world();
 
-	int table[max_procs][max_procs];
-	int row[max_procs];
-
-	int participants = max_procs;
-	assert(world.size() == 8);
+	int table[N][N];
+	
+	assert(world.size() == N);
 
 	if(world.rank() == 0)
-		for(int i = 0; i != participants; ++i)
-			for(int j = 0; j != participants; ++j)
+		for(int i = 0; i != N; ++i)
+			for(int j = 0; j != N; ++j)
 				table[i][j] = i + j;
 
-//	world.scatter(&table[0][0], &table[0][0] + 64, &row[0], 0);
-	world.scatter_from(&row[0], &row[0] + 8, &table[0][0], 0);
+	{
+		int row[N];
+		world.scatter(&table[0][0], &table[0][0] + N*N, &row[0], 0);
+		for(int i = 0; i != N; ++i) assert(row[i] == i + world.rank());
+	}
+	{
+		int row[N];
+		world.scatter_from(&row[0], &row[0] + N, &table[0][0], 0);
+		for(int i = 0; i != N; ++i) assert(row[i] == i + world.rank());
+	}
 
-	for(int i = 0; i != max_procs; ++i) assert(row[i] == i + world.rank());
-
+	return 0;
 }
 
