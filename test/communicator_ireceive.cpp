7f28f77641a91c746a9e471fe02ae0d92b330776
Alfredo Correa <correaa@llnl.gov>
2018-10-22 18:01:28 -0700

    partial patch for ireceive test

--- test/communicator_ireceive.cpp
+++ test/communicator_ireceive.cpp
@@ -1,5 +1,5 @@
 #if COMPILATION_INSTRUCTIONS
-mpic++ -O3 -std=c++14 -Wall -Wextra `#-Wfatal-errors` -fmax-errors=2 $0 -o $0x.x -lpthread -lboost_serialization && time mpirun -n 2 $0x.x $@ && rm -f $0x.x; exit
+mpic++ -O3 -std=c++14 -Wall -Wextra $0 -o $0x.x && mpirun -n 3 $0x.x $@ && rm -f $0x.x; exit
 #endif
 
 #include "../../mpi3/main.hpp"
@@ -12,18 +12,21 @@ using std::cout;
 
 int mpi3::main(int, char*[], mpi3::communicator world){
 
-	assert( world.size() == 2);
+	assert( world.size() > 1 );
 
 	using T = std::string;
-	std::list<T> const cbuffer = {"0", "1", "2"};//(10); std::iota(buffer.begin(), buffer.end(), 0);
-	std::list<T> buffer(3);
+	std::vector<T> const cbuffer = {"0", "1", "2"};//(10); std::iota(buffer.begin(), buffer.end(), 0);
+	std::vector<T> buffer(3); // TODO, test with list
 
 	int right = world.right();
 	int left = world.left();
 	{
 		auto req = world.ireceive(buffer.begin(), left);
 		world.send(cbuffer.begin(), cbuffer.end(), right);
-	} //	req.wait();
+		cout <<"waiting ireceive in rank "<< world.rank() << std::endl;
+		req.wait();
+		cout <<"ireceive completed in rank "<< world.rank() << std::endl;
+	}
 	assert( std::equal(cbuffer.begin(), cbuffer.end(), buffer.begin()) );
 
 	return 0;
