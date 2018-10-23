d8e009fb769d7998069282a4cdcb7a039bb2d8e2
Alfredo Correa <correaa@llnl.gov>
2018-10-22 18:01:46 -0700

    partial patch for ireceive test

--- test/communicator_ireceive.cpp
+++ test/communicator_ireceive.cpp
@@ -15,7 +15,7 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 	assert( world.size() > 1 );
 
 	using T = std::string;
-	std::vector<T> const cbuffer = {"0", "1", "2"};//(10); std::iota(buffer.begin(), buffer.end(), 0);
+	std::vector<T> const cbuffer = {"0", "1", "2"};
 	std::vector<T> buffer(3); // TODO, test with list
 
 	int right = world.right();
