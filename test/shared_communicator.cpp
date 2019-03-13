267688079461b4d5f29e14631ce149281fd92f97
Alfredo Correa <alfredo.correa+work@gmail.com>
2019-03-12 23:21:40 -0700

    example showing that a shared_comm not only is-a but it is also convertible to a normal communicator

--- test/shared_communicator.cpp
+++ test/shared_communicator.cpp
@@ -11,6 +11,9 @@ mpic++ -O3 -std=c++14 -Wall -Wextra -Wfatal-errors $0 -o $0x.x && time mpirun -n
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
+void check_isa_communicator(mpi3::communicator const&){}
+void check_isa_shared_comm (mpi3::shared_communicator const&){}
+
 int mpi3::main(int, char*[], mpi3::communicator world){
 
 	mpi3::ostream wout(world);
@@ -19,6 +22,13 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 	mpi3::shared_communicator node2 = node.split(node.rank() % 2, node.rank());
 
 	wout << "I am rank " << node.rank() << " in " << node.name() << " and rank " << node2.rank() << " in " << node2.name() << std::endl;
+
+	check_isa_shared_comm(node);
+	check_isa_communicator(node);
+
+	mpi3::communicator virtual_node{node};
+	mpi3::communicator virtual_node2;
+	virtual_node2 = node2;
 
 	return 0;
 }
