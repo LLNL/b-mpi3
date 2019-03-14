6fc020b1a5fb569d155cda662bc334581e2fcf17
Alfredo Correa <alfredo.correa+work@gmail.com>
2019-03-13 20:19:32 -0700

    share comm example with numa etc

--- test/shared_communicator.cpp
+++ test/shared_communicator.cpp
@@ -18,10 +18,51 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 
 	mpi3::ostream wout(world);
 
-	mpi3::shared_communicator node = world.split_shared();
+	mpi3::shared_communicator core = world.split_shared(mpi3::communicator_type::core);
+	wout << "I am rank " << core.rank() << " in comm " << core.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator hw = world.split_shared(mpi3::communicator_type::hw_thread);
+	wout << "I am rank " << hw.rank() << " in comm " << hw.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator l1 = world.split_shared(mpi3::communicator_type::l1_cache);
+	wout << "I am rank " << l1.rank() << " in comm " << l1.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator l2 = world.split_shared(mpi3::communicator_type::l2_cache);
+	wout << "I am rank " << l2.rank() << " in comm " << l2.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator l3 = world.split_shared(mpi3::communicator_type::l3_cache);
+	wout << "I am rank " << l3.rank() << " in comm " << l3.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator socket = world.split_shared(mpi3::communicator_type::socket);
+	wout << "I am rank " << socket.rank() << " in comm " << socket.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator numa = world.split_shared(mpi3::communicator_type::numa);
+	wout << "I am rank " << numa.rank() << " in comm " << numa.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator board = world.split_shared(mpi3::communicator_type::board);
+	wout << "I am rank " << board.rank() << " in comm " << board.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator host = world.split_shared(mpi3::communicator_type::host);
+	wout << "I am rank " << host.rank() << " in comm " << host.name() << std::endl;
+	wout << "----" << std::endl;
+
+	mpi3::shared_communicator cu = world.split_shared(mpi3::communicator_type::cu);
+	wout << "I am rank " << cu.rank() << " in comm " << cu.name() << std::endl;
+	wout << "----" << std::endl;
+
+	return 0;
+#if 0
 	mpi3::shared_communicator node2 = node.split(node.rank() % 2, node.rank());
 
-	wout << "I am rank " << node.rank() << " in " << node.name() << " and rank " << node2.rank() << " in " << node2.name() << std::endl;
+	wout << "I am rank " << node.rank() << " in comm " << node.name() << " and rank " << node2.rank() << " in " << node2.name() << std::endl;
 
 	check_isa_shared_comm(node);
 	check_isa_communicator(node);
@@ -36,6 +77,8 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 	mpi3::communicator virtual_node3  = world.split_shared();
 	assert( &virtual_node3 != &world );
 
+	assert( sizeof(mpi3::communicator) == sizeof(mpi3::shared_communicator) );
+#endif
 	return 0;
 }
 
