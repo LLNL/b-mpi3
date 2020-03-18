19067220f02ec932929a1d3f879c2f627ec76e90
Alfredo Correa <alfredo.correa@gmail.com>
2020-03-17 22:16:31 -0700

    removed unused world arg

--- test/shared_mutex.cpp
+++ test/shared_mutex.cpp
@@ -1,4 +1,7 @@
-
+#if COMPILATION_INSTRUCTIONS
+mpic++ $0 -o $0x -lboost_serialization&&mpirun -n 3 $0x&&rm $0x;exit
+#endif
+// © Alfredo Correa 2018-2020
 
 #include "../../mpi3/main.hpp"
 #include "../../mpi3/environment.hpp"
@@ -7,6 +10,6 @@
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
-int mpi3::main(int, char*[], mpi3::communicator world){
+int mpi3::main(int, char*[], mpi3::communicator){
 	return 0;
 }
