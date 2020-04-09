f562f09d6d96c3634b11188aa0873b7af2652b51
Alfredo Correa <alfredo.correa@gmail.com>
2020-04-08 21:50:38 -0700

    ditto

--- test/communicator_all_to_all.cpp
+++ test/communicator_all_to_all.cpp
@@ -1,5 +1,5 @@
 #if COMPILATION_INSTRUCTIONS
-mpic++ -Wall -Wextra -Wfatal-errors $0 -o $0x&&mpirun -n 4 $0x$@&&rm $0x;exit
+mpic++ -Werror=unused-result $0 -o $0x&&mpirun -n 4 $0x$@&&rm $0x;exit
 #endif
 
 #include "../../mpi3/main.hpp"
@@ -17,6 +17,7 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 		world.rank()*10 + 2,
 		world.rank()*10 + 3
 	};
+
 	assert( (int)send_buff.size() == world.size() );
 
 	std::vector<int> recv_buff(world.size(), 99.);
@@ -37,10 +38,20 @@ int mpi3::main(int, char*[], mpi3::communicator world){
 
 	assert( recv_buff == recv_buff2 );
 
-//	for(std::size_t i = 0; i != send_buff.size(); ++i)
-//		world[i] << send_buff[i] >> send_buff[i];
-	world & send_buff;
-	assert( recv_buff == send_buff );
+	{
+		auto const& csend_buff = send_buff;
+		auto const r = world & csend_buff;
+		assert( recv_buff == r );
+	}
+	{
+		auto const& csend_buff = send_buff;
+		auto const r = world(csend_buff);
+		assert( recv_buff == r );
+	}
+	{
+		world & send_buff;
+		assert( recv_buff == send_buff );
+	}
 
 	return 0;
 }
