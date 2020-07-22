91cd725ddce84f37f122f0e99bb6461ff5e4a003
Alfredo Correa <correaa@llnl.gov>
2020-07-21 22:17:05 -0700

    fixed divide test

--- test/communicator_divide.cpp
+++ test/communicator_divide.cpp
@@ -1,14 +1,27 @@
 #if COMPILATION_INSTRUCTIONS
-mpic++ -O3 -Wall -Wextra $0 -o $0x &&mpirun -n 3 valgrind --suppressions=communicator_main.cpp.openmpi.supp $0x&&rm $0x;exit
+mpic++  -Wall -Wextra $0 -o $0x &&mpirun -n 4 valgrind --suppressions=communicator_main.cpp.openmpi.supp $0x&&rm $0x;exit
 #endif
 // © Alfredo Correa 2018-2020
-#include "../../mpi3/communicator.hpp"
+
 #include "../../mpi3/main.hpp"
+#include "../../mpi3/communicator.hpp"
 
 namespace mpi3 = boost::mpi3;
+using std::cout;
 
 int mpi3::main(int, char*[], mpi3::communicator world){
-	assert( world.size() == 3 );
+
+	assert( world.size() == 4 );
+
+	mpi3::communicator fifth = world/3;
+
+	cout << "I am rank " << world.rank() << " in " << world.name() << ", ";
+	if(fifth){
+		cout << "I am also " << fifth.rank() << " in " << fifth.name() << '\n';
+	}else{
+		cout << "I am not in " << fifth.name() << '\n';
+	}
+
 	return 0;
 }
 
