48cd8469950ddcd5e8120d1fa04a101850b6b70a
Alfredo Correa <correaa@llnl.gov>
2020-11-04 13:58:49 -0800

    make nodiscard macro c++17-friendly

--- config/NODISCARD.hpp
+++ config/NODISCARD.hpp
@@ -1,5 +1,5 @@
 #ifdef COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-$CXX $0 -o $0x &&$0x&&rm $0x;exit
+$CXXX $CXXFLAGS $0 -o $0.$X &&$0.$X&&rm $0.$X;exit
 #endif
 // © Alfredo A. Correa 2019-2020
 
@@ -10,20 +10,33 @@ $CXX $0 -o $0x &&$0x&&rm $0x;exit
 #define __has_cpp_attribute(name) 0
 #endif
 
-#ifndef NODISCARD
-#if (__has_cpp_attribute(nodiscard)) && (__cplusplus>=201703L)
-	#define NODISCARD(MsG) [[nodiscard]]
-	#if (__has_cpp_attribute(nodiscard)>=201907) && (__cplusplus>=201703L)
-		#define NODISCARD(MsG) [[nodiscard(MsG)]]
-	#endif
-#elif __has_cpp_attribute(gnu::warn_unused_result)
-	#define NODISCARD(MsG) [[gnu::warn_unused_result]]
+#define MPI3_GET_MACRO(_1,NAME,...) NAME
+
+#if   (__has_cpp_attribute(nodiscard)>=201603L) and not defined(__NVCC__)
+	#define NODISCARD0()    [[nodiscard]]
+	#define NODISCARD1(MsG) [[nodiscard]]
+#elif (__has_cpp_attribute(nodiscard)>=201907L) and not defined(__NVCC__)
+	#define NODISCARD0()    [[nodiscard]]
+	#define NODISCARD1(MsG) [[nodiscard(MsG)]]
+#elif (__has_cpp_attribute(warn_unused_result))
+	#define NODISCARD0()    __attribute__((warn_unused_result))
+	#define NODISCARD1(MsG) __attribute__((warn_unused_result))
 #else
-	#define NODISCARD(MsG)
-#endif
+	#if __INTEL_COMPILER
+		#pragma warning( push,    1028 )
+		#pragma warning( disable: 1028 )
+	#endif
+	#pragma warning "compiler doesn't support no discard warning?"
+	#if __INTEL_COMPILER
+		#pragma warning( pop )
+	#endif
+	#define NODISCARD0()
+	#define NODISCARD1(MsG)
 #endif
 
-#if not __INCLUDE_LEVEL__ // _TEST_MPI3_CONFIG_NODISCARD
+#define NODISCARD(...) MPI3_GET_MACRO(__VA_ARGS__, NODISCARD1, NODISCARD0)(__VA_ARGS__)
+
+#if defined(__INCLUDE_LEVEL__)  and not __INCLUDE_LEVEL__
 
 NODISCARD("because...") int f(){return 5;}
 //[[nodiscard]] int g(){return 5;} // ok in g++ -std=c++14
