d1dfc43f1bb7858104a9ca438638efcaa3e6834b
Alfredo Correa <correaa@llnl.gov>
2020-12-23 14:04:58 -0800

    align code

--- test/process_vector.cpp
+++ test/process_vector.cpp
@@ -1,9 +1,10 @@
 #if COMPILATION_INSTRUCTIONS
-mpicxx -O3 -std=c++14 `#-Wfatal-errors` $0 -o $0x.x -lboost_serialization -lboost_container && time mpirun -np 2 $0x.x $@ && rm -f $0x.x; exit
+mpicxx -O3 -std=c++14 $0 -o $0.x -lboost_serialization -lboost_container&&time mpirun -np 2 $0.x&&rm -f $0.x;exit
 #endif
 
 #include "../main.hpp"
 #include "../process.hpp"
+
 #include <boost/serialization/vector.hpp>
 
 struct long_long{
@@ -21,22 +22,24 @@ using std::cout;
 
 int mpi3::main(int argc, char* argv[], mpi3::communicator world){
 
-	assert(world.size() == 2);
+	assert( world.size() == 2 );
 	long long size = 100000000;
 	switch(world.rank()){
-		case 0:{
-			std::vector<long_long> v(size);
-			std::iota(v.begin(), v.end(), 0.);
+	case 0:
+		{
+			std::vector<long_long> v(size); std::iota(v.begin(), v.end(), 0.);
 		//	assert(std::accumulate(v.begin(), v.end(), 0.) == size*(size-1)/2 );
 			world[1] << v;
-		}break;
-		case 1:{
-			std::vector<long_long> w;
-			world[0] >> w;
-			assert(w.size() == size);
-			assert(w[45].value = 45);
+		}
+		break;
+	case 1:
+		{
+			std::vector<long_long> w; world[0] >> w;
+			assert( w.size() == size );
+			assert( w[45].value = 45 );
 		//	assert(std::accumulate(w.begin(), w.end(), 0.) == size*(size-1)/2 );
-		}break;
+		}
+		break;
 	}
 	return 0;
 
