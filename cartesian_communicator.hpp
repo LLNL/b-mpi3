bc0a03ff5a751815ad957bafdb8293f55c9bf05d
Alfredo Correa <correaa@llnl.gov>
2021-06-15 13:00:13 -0700

    remove copy assignment for cartesian communicator

--- cartesian_communicator.hpp
+++ cartesian_communicator.hpp
@@ -41,6 +41,8 @@ struct cartesian_communicator<dynamic_extent> : communicator{
 
 	[[deprecated("use dimensionality() instead of dimension")]] 
 	int dimension() const{int ret; MPI_Cartdim_get(impl_, &ret); return ret;}
+	
+	cartesian_communicator& operator=(cartesian_communicator const&) = delete;
 
 	int dimensionality() const{int ret; MPI_(Cartdim_get)(impl_, &ret); return ret;}
 	std::vector<int> coordinates() const{
