ba2b494f3a978c1a244a0189426570c31efb0f02
Alfredo Correa <correaa@llnl.gov>
2021-06-17 12:32:03 -0700

    add try catch

--- test/communicator_operator.cpp
+++ test/communicator_operator.cpp
@@ -18,21 +18,26 @@ auto mpi3::main(int/*argc*/, char**/*argv*/, mpi3::communicator world) -> int tr
 	std::vector<double> inbuf(100);
 	std::vector<double> outbuf(100);
 
-	mpi3::request r;
 	switch(world.rank()){
 		case 0: {
 			iota(begin(outbuf), end(outbuf), 0.0);
 			std::this_thread::sleep_for(2s);
 			cout <<"world["<< world.rank() <<"] about to isent"<< std::endl;
-			r = world.isend(begin(outbuf), end(outbuf), 1);
+			mpi3::request r = world.isend(begin(outbuf), end(outbuf), 1);
 			cout <<"comm["<< world.rank() <<"] isent"<< std::endl;
-			r.wait();
+		//	r.wait();
 		}; break;
 		case 1: {
 			cout <<"comm["<< world.rank() <<"] about to ireceive"<< std::endl;
-			r = world.ireceive_n(inbuf.begin(), inbuf.size(), 0);
+			mpi3::request r;//= world.ireceive_n(inbuf.begin(), inbuf.size(), 0);
+			MPI_Irecv(
+				inbuf.data(), inbuf.size(), 
+				detail::basic_datatype<double>{},
+				MPI_ANY_SOURCE, MPI_ANY_TAG, world.get(), &r.impl_
+			);
 			cout <<"comm["<< world.rank() <<"] ireceived"<< std::endl;
-			r.wait();
+			MPI_Wait(&r.impl_, MPI_STATUS_IGNORE);
+		//	r.wait();
 		}; break;
 		default: break;
 	}
