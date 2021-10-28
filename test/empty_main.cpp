ef6416b7e908535d388924b2eddeb5836a236443
Alfredo Correa <correaa@llnl.gov>
2021-10-28 11:58:01 -0700

    remove unused namespace

--- test/empty_main.cpp
+++ test/empty_main.cpp
@@ -5,8 +5,7 @@ mpicxx $0 -o $0x&&mpirun -n 1 $0x $@&&rm $0x;exit
 
 #include "../../mpi3/environment.hpp"
 
-namespace mpi3 = boost::mpi3;
-
+//namespace mpi3 = boost::mpi3;
 //mpi3::environment env;
 
 int main(){
