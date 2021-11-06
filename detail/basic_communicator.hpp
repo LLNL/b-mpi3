bf2c1bd9c360c712f3f4fb37d3f19d52d0d8ec53
Alfredo Correa <correaa@llnl.gov>
2021-11-06 04:57:42 -0700

    whitespace

--- detail/basic_communicator.hpp
+++ detail/basic_communicator.hpp
@@ -45,7 +45,7 @@ class basic_communicator{
 //			if(s != MPI_SUCCESS) throw std::runtime_error("cannot duplicate communicator");
 //		}
 //	}
-	basic_communicator(basic_communicator& other){
+	basic_communicator(basic_communicator& other) {
 		if(MPI_COMM_NULL != other.impl_){
 			int s = MPI_Comm_dup(other.impl_, &impl_);
 			if(s != MPI_SUCCESS) {throw std::runtime_error("cannot duplicate communicator");}
