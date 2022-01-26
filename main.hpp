0a0390354290ccf4ab0336568a689d0cae2c53ac
Alfredo Correa <correaa@llnl.gov>
2022-01-25 19:53:09 -0800

    add global barrier to end program

--- main.hpp
+++ main.hpp
@@ -1,5 +1,6 @@
-// #if COMPILATION// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
-// © Alfredo A. Correa 2018-2021
+// -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4-*-
+// Copyright 2018-2022 Alfredo A. Correa
+
 #ifndef BOOST_MPI3_MAIN_HPP
 #define BOOST_MPI3_MAIN_HPP
 
@@ -18,8 +19,10 @@ static int main(int /*argc*/, char** /*argv*/, boost::mpi3::communicator /*world
 // cppcheck-suppress syntaxError ; bug cppcheck 2.3
 auto main(int argc, char** argv) -> int try {  // NOLINT(misc-definitions-in-headers) : if you include this file you shouldn't have your own `::main`, you should define `boost::mpi3::main(int argc, char** argv, boost::mpi3::communicator world)` instead
 	boost::mpi3::environment env{argc, argv};
-	try{
-		return boost::mpi3::main(argc, argv, /*env.*/ boost::mpi3::environment::get_world_instance());
+	try {
+		int const ret = boost::mpi3::main(argc, argv, /*env.*/ boost::mpi3::environment::get_world_instance());
+		boost::mpi3::environment::get_world_instance().barrier();
+		return ret;
 	} catch(std::exception& e) {
 		if(boost::mpi3::environment::get_world_instance().root()) {std::cerr<<"exception message: "<< e.what() <<"\n\n\n"<<std::endl;}
 		return 1;
@@ -31,24 +34,4 @@ auto main(int argc, char** argv) -> int try {  // NOLINT(misc-definitions-in-hea
 	std::cerr<<"unknown error in MPI pogram"<<std::endl;
 	return 1;
 }
-
-
-#if not __INCLUDE_LEVEL__ // _TEST_BOOST_MPI3_MAIN
-
-#include "../mpi3/version.hpp"
-#include<iostream>
-
-namespace mpi3 = boost::mpi3;
-using std::cout;
-
-int boost::mpi3::main(int argc, char* argv[], mpi3::communicator world) {
-	if(world.rank() == 0) cout<< mpi3::version() <<'\n';
-	mpi3::communicator duo = world < 2;
-	if(duo) cout <<"my rank in comm "<< duo.name() <<" is "<< duo.rank() <<'\n';
-	return 0;
-}
-
 #endif
-#endif
-
-
