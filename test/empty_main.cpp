54db483378aeed989cc7ae19e4e26d52a6300815
Alfredo Correa <correaa@llnl.gov>
2022-01-27 11:51:10 -0800

    empty main should actually have an environment

--- test/empty_main.cpp
+++ test/empty_main.cpp
@@ -2,7 +2,9 @@
 
 #include "../../mpi3/environment.hpp"
 
-//namespace mpi3 = boost::mpi3;
+namespace mpi3 = boost::mpi3;
 //mpi3::environment env;
 
-auto main() -> int{}
+auto main() -> int{
+	mpi3::environment env;
+}
