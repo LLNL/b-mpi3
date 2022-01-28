cb877acc7ba40101b6d30dc6f9cff6b82a635572
Alfredo Correa <correaa@llnl.gov>
2022-01-27 19:02:46 -0800

    finish

--- test/ibarrier.cpp
+++ test/ibarrier.cpp
@@ -2,14 +2,21 @@
 #include "../../mpi3/main.hpp"
 #include "../../mpi3/communicator.hpp"
 
+#include<chrono>
+
 namespace mpi3 = boost::mpi3;
 using std::cout;
 
 auto mpi3::main(int/*argc*/, char**/*argv*/, mpi3::communicator world) -> int try {
 	auto const my_rank = world.rank();
 	mpi3::request r = world.ibarrier();
+
+	using namespace std::literals::chrono_literals;
+	std::this_thread::sleep_for(2s);
+
 	std::cout<<"mpi process "<< my_rank <<" call ibarrier."<< std::endl;
 	r.wait();
+	assert( r.completed() );
 	std::cout<<"mpi process "<< my_rank <<" the barrier is complete."<< std::endl;
 	return 0;
 } catch(...) {return 1;}
