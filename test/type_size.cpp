ee99534ff58aa87f4194648bb47c5bd4ef8720cb
Alfredo Correa <correaa@llnl.gov>
2022-03-31 13:50:02 -0700

    static assert

--- test/type_size.cpp
+++ test/type_size.cpp
@@ -11,11 +11,11 @@ int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) try {
 	{
 		using mpi3::detail::is_basic;
 
-		static_assert(     is_basic<int>{}, "");
-		static_assert(     is_basic<double>{}, "");
-		static_assert(     is_basic<mpi3::detail::float_int>{}, "");
+		static_assert(     is_basic<int>{} );
+		static_assert(     is_basic<double>{} );
+		static_assert(     is_basic<mpi3::detail::float_int>{} );
 
-		static_assert( not is_basic<std::string>{}, "");
+		static_assert( not is_basic<std::string>{} );
 
 		assert( mpi3::detail::basic_datatype<double>{} == MPI_DOUBLE );
 	}
