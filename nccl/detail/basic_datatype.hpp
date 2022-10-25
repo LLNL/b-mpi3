a1048971f56f279ca8a906d79d806c184f4128ff
Alfredo Correa <correaa@llnl.gov>
2022-10-25 13:47:54 -0700

    comments on types

--- nccl/detail/basic_datatype.hpp
+++ nccl/detail/basic_datatype.hpp
@@ -4,7 +4,7 @@
 #ifndef MPI3_NCCL_DETAIL_BASIC_DATATYPE_HPP_
 #define MPI3_NCCL_DETAIL_BASIC_DATATYPE_HPP_
 
-#include <nccl.h>
+#include <nccl.h>  // for ncclDataType_t
 
 namespace boost {
 namespace mpi3 {
@@ -18,24 +18,26 @@ template<> struct basic_datatype_t<int8_t>   : std::integral_constant<ncclDataTy
 template<> struct basic_datatype_t<char>     : std::integral_constant<ncclDataType_t, ncclChar  > {};
 template<> struct basic_datatype_t<uint8_t>  : std::integral_constant<ncclDataType_t, ncclUint8 > {};
 template<> struct basic_datatype_t<int32_t>  : std::integral_constant<ncclDataType_t, ncclInt32 > {};
-	// template<> struct basic_datatype_t<int>      : std::integral_constant<ncclDataType_t, ncclInt   > {};
+	// template<> struct basic_datatype_t<int>      : std::integral_constant<ncclDataType_t, ncclInt   > {};  // is redundant with some other type, int32_t?
 template<> struct basic_datatype_t<uint32_t> : std::integral_constant<ncclDataType_t, ncclUint32> {};
 
 template<> struct basic_datatype_t<int64_t > : std::integral_constant<ncclDataType_t, ncclInt64 > {};
 
-//template<> struct basic_datatype_t<uint64_t> : std::integral_constant<ncclDataType_t, ncclUInt64> {};
+//template<> struct basic_datatype_t<uint64_t> : std::integral_constant<ncclDataType_t, ncclUInt64> {};  // not defined in NCCL 2.13.4
 
-// template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclFloat16 > {};
+// template<> struct basic_datatype_t<std::float16_t > : std::integral_constant<ncclDataType_t, ncclFloat16 > {};  // needs C++23
+
 // template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclHalf > {};
-// template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclFloat32 > {};
+
+// template<> struct basic_datatype_t<std::float32_t > : std::integral_constant<ncclDataType_t, ncclFloat32 > {};  // needs C++23
 
 template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclFloat  > {};
 
-// template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclFloat64 > {};
+// template<> struct basic_datatype_t<std::float64_t> : std::integral_constant<ncclDataType_t, ncclFloat64 > {};  // needs C++23
 
 template<> struct basic_datatype_t<double > : std::integral_constant<ncclDataType_t, ncclDouble > {};
 
-// template<> struct basic_datatype_t<float  > : std::integral_constant<ncclDataType_t, ncclBfloat64 > {};
+// template<> struct basic_datatype_t<std::bfloat16_t> : std::integral_constant<ncclDataType_t, ncclBfloat64 > {};  // needs C++23
 
 template<class T> auto basic_datatype = basic_datatype_t<T>::value;
 
