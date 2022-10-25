9c690fac2323e3d293023fca698738f80bf168ce
Alfredo Correa <correaa@llnl.gov>
2022-10-25 14:11:07 -0700

    do not depend on multi

--- nccl/test/nccl_constructor.cu
+++ nccl/test/nccl_constructor.cu
@@ -1,11 +1,11 @@
 #include "../../../mpi3/nccl/communicator.hpp"
 #include "../../../mpi3/main.hpp"
 
-#include "/home/correaa/prj/alf/boost/multi/include/multi/array.hpp"
 #include <thrust/system/cuda/memory.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
 namespace mpi3 = boost::mpi3;
-namespace multi = boost::multi;
 
 int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) {
 	assert(world.size() == 4);
@@ -16,11 +16,11 @@ int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) {
 
 	mpi3::nccl::communicator magnesium{hemi};
 
-//  multi::array<int64_t, 1, thrust::cuda::universal_allocator<int64_t>> A({1000}, world.rank());
-	multi::array<int64_t, 1, thrust::cuda::allocator<int64_t>> A({1000}, world.rank());
+//  thust::device_vector<int64_t, thrust::cuda::universal_allocator<int64_t>> A(1000, world.rank());
+	thrust::device_vector<int64_t, thrust::cuda::allocator<int64_t>> A(1000, world.rank());
 
-	magnesium.all_reduce_n(A.data_elements(), A.num_elements(), A.data_elements());
-	multi::array<int64_t, 1> H = A;
+	magnesium.all_reduce_n(A.data(), A.size(), A.data());
+	thrust::host_vector<int64_t> H = A;
 
 	std::cout<<"[rank"<< world.rank() <<"] result:"<< H[0] <<std::endl;
 
