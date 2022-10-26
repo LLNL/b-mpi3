7252d05b2d03afd129e1d7f83ce60bc827fb6827
Alfredo A. Correa <correaa@llnl.gov>
2022-10-25 21:44:17 -0700

    tidy example

--- nccl/test/nccl_constructor.cu
+++ nccl/test/nccl_constructor.cu
@@ -10,19 +10,29 @@ namespace mpi3 = boost::mpi3;
 int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) {
 	assert(world.size() == 4);
 
-//	cudaSetDevice(world_rank); // GPU N binds to MPI rank N
+	cudaSetDevice(world.rank());
 
 	auto hemi = world / 2;
 
+//	if(world.rank() < 2) {  // this conditional is to show that it works in single subcomm
+
 	mpi3::nccl::communicator magnesium{hemi};
 
-//  thust::device_vector<int64_t, thrust::cuda::universal_allocator<int64_t>> A(1000, world.rank());
+	//  thust::device_vector<int64_t, thrust::cuda::universal_allocator<int64_t>> A(1000, world.rank());
 	thrust::device_vector<int64_t, thrust::cuda::allocator<int64_t>> A(1000, world.rank());
 
 	magnesium.all_reduce_n(A.data(), A.size(), A.data());
 	thrust::host_vector<int64_t> H = A;
 
 	std::cout<<"[rank"<< world.rank() <<"] result:"<< H[0] <<std::endl;
+	
+	switch(world.rank()) {
+	       case 0: assert(H[0] == 1);
+	break; case 1: assert(H[0] == 1);
+	break; case 2: assert(H[0] == 5);
+	break; case 3: assert(H[0] == 5);
+	}
 
+//	}
 	return 0;
 }
