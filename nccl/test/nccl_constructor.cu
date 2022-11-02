c9a2acf409881d1bde6b0c936970ad3e6389e83c
Alfredo Correa <correaa@llnl.gov>
2022-11-02 00:10:45 -0700

    capitalize variable

--- nccl/test/nccl_constructor.cu
+++ nccl/test/nccl_constructor.cu
@@ -12,14 +12,14 @@ namespace mpi3 = boost::mpi3;
 int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator WORLD) {
 	assert(WORLD.size() == 4);  
 
-	cudaSetDevice(world.rank());
+	cudaSetDevice(WORLD.rank());
 
 	auto HEMI = WORLD < 2;
 
 	mpi3::nccl::communicator magnesium{HEMI};
 	assert(magnesium.rank() == HEMI.rank());
 
-	using T = thrust::complex<double>;  // int64_t;
+	using T = int64_t;  // thrust::complex<double>;  // int64_t;
 //  thust::device_vector<T, thrust::cuda::universal_allocator<T>> A(1000, world.rank());
 	thrust::device_vector<T, thrust::cuda::allocator<T>> A(1000, T{1.*WORLD.rank()});
 
