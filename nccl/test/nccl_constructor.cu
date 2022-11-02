156d80db7b341d93bb89112e53a5275e8bb280b6
Alfredo Correa <correaa@llnl.gov>
2022-11-01 21:48:07 -0700

    nccl cpu broadcast test

--- nccl/test/nccl_constructor.cu
+++ nccl/test/nccl_constructor.cu
@@ -9,28 +9,29 @@
 
 namespace mpi3 = boost::mpi3;
 
-int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) {
-	assert(world.size() == 4);
+int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator WORLD) {
+	assert(WORLD.size() == 4);  
 
 //	cudaSetDevice(world_rank); // GPU N binds to MPI rank N
 
-	auto hemi = world / 2;
+	auto HEMI = WORLD < 2;
 
-	mpi3::nccl::communicator magnesium{hemi};
+	mpi3::nccl::communicator magnesium{HEMI};
+	assert(magnesium.rank() == HEMI.rank());
 
 	using T = thrust::complex<double>;  // int64_t;
 //  thust::device_vector<T, thrust::cuda::universal_allocator<T>> A(1000, world.rank());
-	thrust::device_vector<T, thrust::cuda::allocator<T>> A(1000, T{1.*world.rank()});
+	thrust::device_vector<T, thrust::cuda::allocator<T>> A(1000, T{1.*WORLD.rank()});
 
 	magnesium.all_reduce_n(A.data(), A.size(), A.data());
 	thrust::host_vector<T> H = A;
 
-	std::cout<<"[rank"<< world.rank() <<"] result:"<< H[0] <<std::endl;
+	std::cout<<"[WORLD rank"<< WORLD.rank() <<" HEMI rank"<< HEMI.rank() <<"] result:"<< H[0] <<std::endl;
 
-//	assert( magnesium.count() == 2 );
+//  assert( magnesium.count() == 2 );
 
-//	auto magnesium2 = std::move(magnesium);
-//	assert( magnesium2.count() == 2 );
+//  auto magnesium2 = std::move(magnesium);
+//  assert( magnesium2.count() == 2 );
 
 	switch(magnesium.rank()) {
 	case 0: {
@@ -42,6 +43,12 @@ int mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) {
 		assert( A == B );
 	}
 	}
+
+	int i = 0;
+	if(not magnesium.rank()) { i = 99; }
+
+	magnesium.broadcast_n(&i, 1);
+	assert( i == 99 );
 
 	return 0;
 }
