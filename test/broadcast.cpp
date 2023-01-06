02958e120458fae71bf70248b48d0822fc58939d
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-01-05 18:12:18 -0800

    reorder incls

--- test/broadcast.cpp
+++ test/broadcast.cpp
@@ -1,8 +1,8 @@
 // -*-indent-tabs-mode:t;c-basic-offset:4;tab-width:4;autowrap:nil;-*-
 // Copyright 2023 Alfredo A. Correa
 
-#include <mpi3/main.hpp>
 #include <mpi3/communicator.hpp>
+#include <mpi3/main.hpp>
 
 #include<multi/array.hpp>
 
