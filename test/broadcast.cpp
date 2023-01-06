1908ae526c22a31e998e225641612b67078b5e24
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-01-06 00:49:40 -0800

    scope namespace alias

--- test/broadcast.cpp
+++ test/broadcast.cpp
@@ -7,7 +7,6 @@
 #include<multi/array.hpp>
 
 namespace mpi3 = boost::mpi3;
-namespace multi = boost::multi;
 
 auto mpi3::main(int/*argc*/, char**/*argv*/, mpi3::communicator world) -> int try {
 
@@ -20,6 +19,7 @@ auto mpi3::main(int/*argc*/, char**/*argv*/, mpi3::communicator world) -> int tr
 		assert( std::find_if(v.cbegin(), v.cend(), [](auto& e) {return e != 3.14;}) == v.cend() );
 	}
 	{
+		namespace multi = boost::multi;
 		auto arr = multi::array<double, 1>(10, world.root()? 3.14 : 99.9);
 
 		auto it = world.broadcast_n(arr.begin(), 10);
