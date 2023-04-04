67f0c214ac904f8541c2acefe73e07d72704ad6f
Xavier Andrade <xavier@tddft.org>
2023-04-03 17:49:21 -0700

    Clang needs an explicit signed to unsigned cast.

--- include/mpi3/detail/package.hpp
+++ include/mpi3/detail/package.hpp
@@ -24,7 +24,7 @@ struct package : buffer {
 
  public:
 	explicit package(communicator& comm, buffer::size_type n = 0)
-	: buffer{n}, bcomm_{reinterpret_cast<basic_communicator&>(comm)} {  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) TODO(correaa) break cyclic dependency of classes
+		: buffer{static_cast<std::size_t>(n)}, bcomm_{reinterpret_cast<basic_communicator&>(comm)} {  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) TODO(correaa) break cyclic dependency of classes
 		reserve(n);
 	}
 	package(package const&) = delete;
