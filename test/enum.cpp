fb225c7fd5b90ca1f4e63fafc8e6abfcfc4fd9bb
Alfredo Correa <alfredo.correa@gmail.com>
2023-05-31 00:59:41 -0700

    change casing of names

--- test/enum.cpp
+++ test/enum.cpp
@@ -2,8 +2,8 @@
 // Copyright 2023 Alfredo A. Correa
 
 #include <mpi3/communicator.hpp>
-#include <mpi3/process.hpp>
 #include <mpi3/main.hpp>
+#include <mpi3/process.hpp>
 
 #include <boost/variant.hpp>
 
@@ -13,111 +13,102 @@
 
 namespace mpi3 = boost::mpi3;
 
-struct WorkerStatus {
-    int        createdEvents;
-    int        skippedEvents;
-    int        finishedEvents;
-};
-
-namespace Error {
-enum Code { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
+namespace error_ns {
+enum code { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
 }
 
-namespace ErrorLong {
-enum Code : unsigned long { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
+namespace error_long_ns {
+enum code : unsigned long { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
 }
 
-namespace ErrorClass {
-enum class Code { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
+namespace error_class_ns {
+enum class code { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
 }
-
-typedef unsigned long code_t;
-enum ErrorCode : code_t { FAILURE = 0, SUCCESS = 1, RECOVERABLE = 2 };
 
 auto mpi3::main(int /*argc*/, char** /*argv*/, mpi3::communicator world) -> int try {
 	assert(world.size() > 1);
 
 	switch(world.rank()) {
 	break; case 0: {
-		Error::Code const ec = Error::SUCCESS;
-		world.send_n(&reinterpret_cast<int const&>(ec), 1, 1);
+		error_ns::code const ec = error_ns::SUCCESS;
+		world.send_n(&reinterpret_cast<int const&>(ec), 1, 1);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) testing not recommended brute force method
 	};
 	break; case 1: {
-		Error::Code ec;
-		world.receive_n(&reinterpret_cast<int&>(ec), 1, 0);
-		assert(ec == Error::SUCCESS);
+		error_ns::code ec{};
+		world.receive_n(&reinterpret_cast<int&>(ec), 1, 0);  // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) testing not recommended brute force method
+		assert(ec == error_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		Error::Code const ec = Error::SUCCESS;
+		error_ns::code const ec = error_ns::SUCCESS;
 		world.send_n(&ec, 1, 1);
 	};
 	break; case 1: {
-		Error::Code ec;
+		error_ns::code ec{};
 		world.receive_n(&ec, 1, 0);
-		assert(ec == Error::SUCCESS);
+		assert(ec == error_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		Error::Code const ec = Error::SUCCESS;
+		error_ns::code const ec = error_ns::SUCCESS;
 		world.send_n(&ec, 1, 1);
 	};
 	break; case 1: {
-		Error::Code ec;
+		error_ns::code ec{};
 		world.receive_n(&ec, 1);
-		assert(ec == Error::SUCCESS);
+		assert(ec == error_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		Error::Code const ec = Error::SUCCESS;
+		error_ns::code const ec = error_ns::SUCCESS;
 		world[1] << ec;
 	};
 	break; case 1: {
-		Error::Code ec;
+		error_ns::code ec{};
 		world[0] >> ec;
-		assert(ec == Error::SUCCESS);
+		assert(ec == error_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		Error::Code const ec = Error::SUCCESS;
+		error_ns::code const ec = error_ns::SUCCESS;
 		world[1] << ec;
 	};
 	break; case 1: {
-		Error::Code ec;
+		error_ns::code ec{};
 		world >> ec;
-		assert(ec == Error::SUCCESS);
+		assert(ec == error_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		ErrorLong::Code const ec = ErrorLong::SUCCESS;
+		error_long_ns::code const ec = error_long_ns::SUCCESS;
 		world[1] << ec;
 	};
 	break; case 1: {
-		ErrorLong::Code ec;
+		error_long_ns::code ec{};
 		world >> ec;
-		assert(ec == ErrorLong::SUCCESS);
+		assert(ec == error_long_ns::SUCCESS);
 	};
 	}
 
 	switch(world.rank()) {
 	break; case 0: {
-		ErrorClass::Code const ec = ErrorClass::Code::SUCCESS;
+		error_class_ns::code const ec = error_class_ns::code::SUCCESS;
 		world[1] << ec;
 	};
 	break; case 1: {
-		ErrorClass::Code ec;
+		error_class_ns::code ec{};
 		world >> ec;
-		assert(ec == ErrorClass::Code::SUCCESS);
+		assert(ec == error_class_ns::code::SUCCESS);
 	};
 	}
 
