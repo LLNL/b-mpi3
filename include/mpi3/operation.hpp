21d8966e8a874663103fd50a23ac759f3534caac
Alfredo Correa <alfredo.correa+work@gmail.com>
2024-06-02 09:52:03 +0000

    Update operation.hpp

--- include/mpi3/operation.hpp
+++ include/mpi3/operation.hpp
@@ -27,7 +27,9 @@ struct builtin_operation;
 template<class T, class P = T*>
 struct operation : detail::nondefault_handle<operation<T>, MPI_Op, MPI_Op_free> { // user_operation, operator_ . operator is a C++ keyword
 	using base = typename detail::nondefault_handle<operation<T>, MPI_Op, MPI_Op_free>;
-	using typename base::nondefault_handle;
+	// using typename base::nondefault_handle;
+	using nondefault_handle = typename base::nondefault_handle;
+
 	operation() = delete;
 
 	template<class Op>
